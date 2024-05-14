b5dcbdaf386352ed11fa66c936dbabeb2cb25af2
yakuphantonintshi <yakupha58756@gmail.com>
2024-05-14 14:21:12 +0200

    Add File

---
+++ scripts/style.js
@@ -0,0 +1,23 @@
+//Question 1
+let firstName = "Yakupha"
+let lastName = "Ntonintshi"
+
+//Question 2
+console.log(firstName + '' + lastName);
+
+//Question 3
+let numberVar = 5;
+
+//Question 4
+console.log(numberVar * 5);
+
+//Question 5
+const firstNames = "Yakupha"
+firstNames = "Ntonintshi"
+
+//style.js:16 Uncaught TypeError: Assignment to constant variable. at style.js:16:12
+
+
+//It gave me an error because you use const when you are not going to chajunge the value of your variable, and here we already had a variable called first name and we use let meaning we cannot change to const
+
+ 
